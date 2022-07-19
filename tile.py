12d674bd4af4f6fdf6385dafd538ecf3fc3a92ac
James Rodgers <jms.rdgrs@gmail.com>
2022-07-19 00:09:16 +0000

    added blank tile

--- tile.py
+++ tile.py
@@ -17,7 +17,7 @@ class Tile:
           "i": 1, "j": 8, "k": 5, "l": 1, "m": 3, "n": 1,
           "o": 1, "p": 3,  "q": 10, "r": 1, "s": 1, "t": 1,
           "u": 1,"v": 4, "w": 4, "x": 8,  "y": 4, 
-       "z": 10}
+       "z": 10, "": 0}
     """Tile class"""
     def __init__(self,tile_letter, tile_score):
 
