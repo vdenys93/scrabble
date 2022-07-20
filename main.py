15016400a46440dbb772301396b040d7dc046020
Viktoriia Denys <viktoriia.denys@gmail.com>
2022-07-20 02:33:34 +0000

    Upload New File

---
+++ main.py
@@ -0,0 +1,42 @@
+import pygame
+import sys
+from scrabble.constants import DISPLAY_WIDTH, DISPLAY_HEIGHT, TAN
+from scrabble.board import Board
+
+FPS = 60
+WIN = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+pygame.display.set_caption("Scrabble")
+
+pygame.init()
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    board = Board()
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            # On click event to see if the mouse clicks on a square
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+
+        # draw a blank screen
+        # WIN.fill(TAN)
+
+        # Redraw the screen
+        board.draw_squares(WIN)
+        pygame.display.update()
+
+        # Make the most recently drawn screen visible
+        # pygame.display.flip()
+
+    pygame.quit()
+    sys.exit()
+
+
+main()
